b0dc90e62f9f00c154bacf1e46d971c81f18d961
ohs70007 <teardrop0529@docomo.ne.jp>
2019-09-19 12:06:27 +0900

    料金更新画面編集

--- master/feeList.php
+++ master/feeList.php
@@ -24,7 +24,7 @@
 
     <div id="main_box">
 
-        <p><a href="#">新規登録→</a></p>
+        <p><a href="feeAdd.php">新規登録→</a></p>
         <table border="1px">
             <tr>
                 <th>料金ID</th>
